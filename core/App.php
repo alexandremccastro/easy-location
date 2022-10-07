e0098254b282e71d138639a25d91064ea89c8f7b
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2022-10-07 00:12:24 -0300

    feat: add helper functions and ability to load files dinamically

--- core/App.php
+++ core/App.php
@@ -9,17 +9,22 @@ abstract class App
 {
   public static function serve($requestURI): void
   {
-    $routes = scandir('routes');
-
-    foreach ($routes as $route) {
-      $filePath = join('/', ['routes', $route]);
-      if (is_file($filePath)) @require_once $filePath;
-    }
-
     try {
+      self::loadResources('helpers');
+      self::loadResources('routes');
       Route::dispatch($requestURI);
     } catch (HttpException|\Exception $e) {
       echo $e->getMessage();
+    }
+  }
+
+  private static function loadResources(string $directory)
+  {
+    $routes = scandir($directory);
+
+    foreach ($routes as $route) {
+      $filePath = join('/', [$directory, $route]);
+      if (is_file($filePath)) @require_once $filePath;
     }
   }
 }
---
+++ helpers/default.php
@@ -0,0 +1,31 @@
+<?php
+
+use Core\Helper\View;
+use Core\Http\Request;
+
+function redirect(string $uri)
+{
+  header("Location: $uri");
+}
+
+function view($fileName, $params)
+{
+  return View::render($fileName, $params);
+}
+
+function request(): object
+{
+  return Request::getInstance();
+}
+
+function formatMoney($value, $symbol = null): string
+{
+  if (is_numeric($value)) return number_format($value, 2) . " $symbol";
+  return $value;
+}
+
+function formatKM($value): string
+{
+  if (is_numeric($value)) return number_format($value, 2) . ' KM';
+  return $value;
+}
